040fa5f71f9379e840b6b00ea8ca62a5c52c08ee
Mykola Zahanych <m.zahanych@gmail.com>
2021-12-10 16:28:30 +0100

    page home, install vue router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -83,14 +83,22 @@
         >
       </li>
     </ul>
+    <div>{{ sourceData }}</div>
   </div>
 </template>
 
 <script>
+import sourceData from "@/data.json";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      sourceData: sourceData,
+    };
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PageHome from "@/components/PageHome";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [{ path: "/", name: "Home", component: PageHome }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const forumApp = createApp(App);
+forumApp.use(router);
+createApp(App).mount("#app");
