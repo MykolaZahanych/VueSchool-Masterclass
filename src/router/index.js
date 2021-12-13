cbdc5441cbbd4acc242f37748685dd13d01e00e2
Mykola Zahanych <m.zahanych@gmail.com>
2021-12-13 17:26:56 +0100

    edit Thread

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Forum from '@/pages/Forum'
 import Category from '@/pages/Category'
 import Profile from '@/pages/Profile'
 import ThreadCreate from '@/pages/ThreadCreate'
+import ThreadEdit from '@/pages/ThreadEdit'
 import NotFound from '@/pages/NotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 import sourceData from '@/data.json'
@@ -58,9 +59,15 @@ const routes = [
     props: true
   },
   {
-    path: '/form/:forumId/thread/create',
+    path: '/forum/:forumId/thread/create',
     name: 'ThreadCreate',
     component: ThreadCreate,
+    props: true
+  },
+  {
+    path: '/thread/:id/edit',
+    name: 'ThreadEdit',
+    component: ThreadEdit,
     props: true
   },
   {
