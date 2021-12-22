7e54508fea2ecb6bb123b7573af9a983d05aaed6
Mykola Zahanych <m.zahanych@gmail.com>
2021-12-22 12:01:31 +0100

    store / update thread

--- src/components/AppDate.vue
+++ src/components/AppDate.vue
@@ -1,6 +1,6 @@
 <template>
-  <span :title="hummanFriendlyDate()">
-    {{ diffForHumans() }}
+  <span :title="hummanFriendlyDate">
+    {{ diffForHumans }}
   </span>
 </template>
 
@@ -15,15 +15,18 @@ export default {
   props: {
     timestamp: {
       required: true,
-      type: Number
+      type: [Number, Object]
     }
   },
-  methods: {
+  computed: {
+    normalizedTimestamp () {
+      return this.timestamp?.seconds || this.timestamp
+    },
     diffForHumans () {
-      return dayjs.unix(this.timestamp).fromNow()
+      return dayjs.unix(this.normalizedTimestamp).fromNow()
     },
     hummanFriendlyDate () {
-      return dayjs.unix(this.timestamp).format('llll')
+      return dayjs.unix(this.normalizedTimestamp).format('llll')
     }
   }
 }
