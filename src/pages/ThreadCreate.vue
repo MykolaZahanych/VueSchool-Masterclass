2d33c1b13853050f4e5c6a080f153455a1d64083
Mykola Zahanych <m.zahanych@gmail.com>
2021-12-24 16:54:18 +0100

    Loading Indicators

--- src/pages/ThreadCreate.vue
+++ src/pages/ThreadCreate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="forum" class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
     <h1>
       Create new thread in <i>{{ forum?.name }}</i>
     </h1>
@@ -11,14 +11,17 @@
 import ThreadEditor from '@/components/ThreadEditor'
 import { findById } from '@/helpers'
 import { mapActions } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 
 export default {
   components: { ThreadEditor },
+  mixins: [asyncDataStatus],
   props: {
     forumId: { type: String, required: true }
   },
-  created () {
-    this.fetchForum({ id: this.forumId })
+  async created () {
+    await this.fetchForum({ id: this.forumId })
+    this.asyncDataStatus_fetched()
   },
   computed: {
     forum () {
