b0f759d454da45e4784324eebd0b51e15c8c42f6
Mykola Zahanych <m.zahanych@gmail.com>
2021-12-26 21:07:50 +0100

    dynamic routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,51 @@
-import Home from '@/pages/Home'
-import ThreadShow from '@/pages/ThreadShow'
-import Forum from '@/pages/Forum'
-import Category from '@/pages/Category'
-import Profile from '@/pages/Profile'
-import ThreadCreate from '@/pages/ThreadCreate'
-import ThreadEdit from '@/pages/ThreadEdit'
-import Register from '@/pages/Register'
-import SignIn from '@/pages/SignIn'
-import NotFound from '@/pages/NotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 import { findById } from '@/helpers'
 import store from '@/store'
-
 const routes = [
-  { path: '/', name: 'Home', component: Home },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "Home" */ '@/pages/Home')
+  },
+  {
+    path: '/me',
+    name: 'Profile',
+    component: () =>
+      import(/* webpackChunkName: "Profile" */ '@/pages/Profile'),
+    meta: { toTop: true, smoothScroll: true, requiresAuth: true }
+  },
+  {
+    path: '/me/edit',
+    name: 'ProfileEdit',
+    component: () =>
+      import(/* webpackChunkName: "Profile" */ '@/pages/Profile'),
+    props: { edit: true },
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/category/:id',
+    name: 'Category',
+    component: () =>
+      import(/* webpackChunkName: "Category" */ '@/pages/Category'),
+    props: true
+  },
+  {
+    path: '/forum/:id',
+    name: 'Forum',
+    component: () => import(/* webpackChunkName: "Forum" */ '@/pages/Forum'),
+    props: true
+  },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: ThreadShow,
+    component: () =>
+      import(/* webpackChunkName: "ThreadShow" */ '@/pages/ThreadShow'),
     props: true,
     async beforeEnter (to, from, next) {
-      await store.dispatch('threads/fetchThread', { id: to.params.id, once: true })
+      await store.dispatch('threads/fetchThread', {
+        id: to.params.id,
+        once: true
+      })
       const threadExists = findById(store.state.threads.items, to.params.id)
       if (threadExists) {
         return next()
@@ -36,54 +61,32 @@ const routes = [
     }
   },
   {
-    path: '/me',
-    name: 'Profile',
-    component: Profile,
-    meta: { toTop: true, smoothScroll: true, requiresAuth: true }
-  },
-  {
-    path: '/me/edit',
-    name: 'ProfileEdit',
-    component: Profile,
-    props: { edit: true },
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/category/:id',
-    name: 'Category',
-    component: Category,
-    props: true
-  },
-  {
-    path: '/forum/:id',
-    name: 'Forum',
-    component: Forum,
-    props: true
-  },
-  {
     path: '/forum/:forumId/thread/create',
     name: 'ThreadCreate',
-    component: ThreadCreate,
+    component: () =>
+      import(/* webpackChunkName: "ThreadCreate" */ '@/pages/ThreadCreate'),
     props: true,
     meta: { requiresAuth: true }
   },
   {
     path: '/thread/:id/edit',
     name: 'ThreadEdit',
-    component: ThreadEdit,
+    component: () =>
+      import(/* webpackChunkName: "ThreadEdit" */ '@/pages/ThreadEdit'),
     props: true,
     meta: { requiresAuth: true }
   },
   {
     path: '/register',
     name: 'Register',
-    component: Register,
+    component: () =>
+      import(/* webpackChunkName: "Register" */ '@/pages/Register'),
     meta: { requiresGuest: true }
   },
   {
     path: '/signin',
     name: 'SignIn',
-    component: SignIn,
+    component: () => import(/* webpackChunkName: "SignIn" */ '@/pages/SignIn'),
     meta: { requiresGuest: true }
   },
   {
@@ -97,10 +100,10 @@ const routes = [
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: NotFound
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ '@/pages/NotFound')
   }
 ]
-
 const router = createRouter({
   history: createWebHistory(),
   routes,
@@ -111,9 +114,10 @@ const router = createRouter({
     return scroll
   }
 })
-
 router.afterEach(() => {
-  store.dispatch('clearItems', { modules: ['categories', 'forums', 'posts', 'threads'] })
+  store.dispatch('clearItems', {
+    modules: ['categories', 'forums', 'posts', 'threads']
+  })
 })
 
 router.beforeEach(async (to, from) => {
